019c72fc1cc39ef6f26ea47ff001c31f72308d0e
Timur Guseynov <me@timurguseynov.com>
2023-12-13 12:59:38 +0300

    add not waiting test

--- main_test.go
+++ main_test.go
@@ -10,22 +10,21 @@ import (
     "github.com/stretchr/testify/assert"
 )
 
-func TestFlow(t *testing.T) {
+var tests = []struct {
+    name                  string
+    totalTasks            int
+    totalTasksInitialized int
+    totalSucceeded        int
+    totalFailed           int
+    f                     failingFunc
+}{
+    {"no failingFunc provided", 10, 0, 0, 0, nil},
+    {"no tasks", 0, 0, 0, 0, failingFuncAllFail},
+    {"all tasks succeed", 10, 10, 10, 0, failingFuncAllSucceed},
+    {"all tasks fail", 10, 10, 0, 10, failingFuncAllFail},
+}
 
-    tests := []struct {
-        name                  string
-        totalTasks            int
-        totalTasksInitialized int
-        totalSucceeded        int
-        totalFailed           int
-        f                     failingFunc
-    }{
-        {"no failingFunc provided", 10, 0, 0, 0, nil},
-        {"no tasks", 0, 0, 0, 0, failingFuncAllFail},
-        {"all tasks succeed", 10, 10, 10, 0, failingFuncAllSucceed},
-        {"all tasks fail", 10, 10, 0, 10, failingFuncAllFail},
-    }
-
+func TestStepByStep(t *testing.T) {
     t.Parallel()
 
     for _, tt := range tests {
@@ -64,7 +63,32 @@ func TestFlow(t *testing.T) {
             assert.Equal(t, tt.totalFailed, len(errors), "wrong number of result errors")
         })
     }
+}
 
+func TestFlow(t *testing.T) {
+    t.Parallel()
+
+    for _, tt := range tests {
+
+        t.Run(tt.name, func(t *testing.T) {
+            ctx, cancel, wg := setup(30 * time.Second)
+            defer cancel()
+
+            appErrCh := testAppErrorHandler(ctx, t)
+
+            creatorCh := creator(ctx, wg, appErrCh, tt.totalTasks, tt.f)
+            workerCh := worker(ctx, wg, creatorCh, tt.totalTasks)
+            succeededCh, failedCh := sorter(ctx, wg, workerCh, tt.totalTasks)
+            tasksCollectedCh := tasksCollector(ctx, wg, succeededCh)
+            errorsCollectedCh := errorsCollector(ctx, wg, failedCh)
+
+            tasks := extractTasks(tasksCollectedCh)
+            assert.Equal(t, tt.totalSucceeded, len(tasks), "wrong number of result tasks")
+
+            errors := extractErrors(errorsCollectedCh)
+            assert.Equal(t, tt.totalFailed, len(errors), "wrong number of result errors")
+        })
+    }
 }
 
 func setup(timeout time.Duration) (context.Context, context.CancelFunc, *sync.WaitGroup) {
